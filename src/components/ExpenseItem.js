afb53916737a5bfa8816744a963ba798dd54da49
Amrit Sen <senamrit7@gmail.com>
2022-03-02 14:20:09 +0530

    Implemented Props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,18 @@
-function ExpenseItem() {
-  return <h2>Expense Item!</h2>;
+import "./ExpenseItem.css";
+function ExpenseItem(props) {
+  //const expenseDate = new Date(2022, 2, 1);
+  //const expenseTitle = "Car Insurance";
+  //const expenseAmount = 249.67;
+
+  return (
+    <div class="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div class="expense-item__description">
+        <h2>{props.title}</h2>
+        <div class="expense-item__price">{props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;

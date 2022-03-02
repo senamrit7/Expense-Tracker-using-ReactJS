9e363d497255bd02659b8bdb1b89ae85626a4f5b
Amrit Sen <senamrit7@gmail.com>
2022-03-02 15:58:30 +0530

    Added ExpenseDate Files - Modified ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,18 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 function ExpenseItem(props) {
-  //const expenseDate = new Date(2022, 2, 1);
-  //const expenseTitle = "Car Insurance";
-  //const expenseAmount = 249.67;
+  // const expenseDate = new Date(2022, 2, 1);
+  // const expenseTitle = "Car Insurance";
+  // const expenseAmount = 249.67;
+
+  // Moved to ExpenseDate.js
+  // const month = props.date.toLocaleString("en-US", { month: "long" });
+  // const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  // const year = props.date.getFullYear();
 
   return (
     <div class="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div class="expense-item__description">
         <h2>{props.title}</h2>
         <div class="expense-item__price">{props.amount}</div>
